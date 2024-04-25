067a12919815090086e82a2e7dd4bcc79c3dbc0c
Sam Seaborn <furrukh.jamal@gmail.com>
2024-04-25 16:53:16 +0500

    basic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect, useRef } from "react";
+import * as poseDetection from "@tensorflow-models/pose-detection";
+import * as tf from "@tensorflow/tfjs-core";
+// Register one of the TF.js backends.
+import "@tensorflow/tfjs-backend-webgl";
+// import '@tensorflow/tfjs-backend-wasm';
+
+import testImage from "./testImage.jpg";
 
 function App() {
+  const [model, setModel] = useState(null);
+  const imageRef = useRef(null);
+  const [poses, setPoses] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      await tf.ready();
+      tf.backend("webgpu");
+
+      const detectorConfig = {
+        modelType: poseDetection.movenet.modelType.SINGLEPOSE_LIGHTNING,
+        enableTracking: true,
+        trackerType: poseDetection.TrackerType.BoundingBox,
+      };
+      const detector = await poseDetection.createDetector(
+        poseDetection.SupportedModels.MoveNet,
+        detectorConfig
+      );
+      setModel(detector);
+      // console.log("detector : ", detector);
+    })();
+  }, []);
+
+  useEffect(() => {
+    // console.log("model : ", model);
+    if (model) {
+      (async () => {
+        let exampleInput = tf.zeros([1, 192, 192, 3]);
+
+        const img = new Image();
+        img.onload = function () {
+          const width = this.naturalWidth;
+          const height = this.naturalHeight;
+
+          console.log(`width : ${width} , height : ${height}`);
+        };
+
+        img.src = testImage;
+
+        const p = await model.estimatePoses(imageRef.current);
+        // console.log("poses are : ", poses);
+        setPoses(p[0].keypoints);
+      })();
+    }
+  }, [model]);
+
+  useEffect(() => {
+    console.log("poses : ", poses);
+  }, [poses]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>TensorFLow JS Movenet Model</h1>
+      {model ? <p>Model Loaded</p> : <p>Model is loading ...</p>}
+      <div className="imageContainer">
+        <img ref={imageRef} src={testImage} width="640" height="360" />
+        {poses.map((pose, index) => (
+          <div
+            key={index}
+            className="posePointDot"
+            style={{
+              top: parseFloat(pose["y"]) - 2,
+              left: parseFloat(pose["x"]) - 2,
+            }}
+          ></div>
+        ))}
+      </div>
     </div>
   );
 }
